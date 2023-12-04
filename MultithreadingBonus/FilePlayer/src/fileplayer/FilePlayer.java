fa02782b720150ff92ac357c359dcbb502c4778f
Shakebuddin <145050577+Shakebuddin@users.noreply.github.com>
2023-12-04 14:15:00 -0500

    Multithreading

---
+++ MultithreadingBonus/FilePlayer/src/fileplayer/FilePlayer.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fileplayer;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+/**
+ *
+ * @author Shakeb
+ */
+public class FilePlayer {
+        public void play(String filePath) {
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+                        Thread1.sleep(clip.getMicrosecondLength()/1500);
+                        
+
+		} catch (Exception e) {
+			System.out.println("Error with playing sound.");
+			e.printStackTrace();
+		}
+
+	}
+    public static void main(String[] args) {
+     Thread1 t1 = new Thread1();
+        t1.start(); 
+        Thread2 t2 = new Thread2();
+        t2.start();
+        
+    }
+  }
+    
+    
+
